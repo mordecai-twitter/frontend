b900c9f43d9afaaec82be2c03a48b9db04e1c832
Samuele Marro <samuelemarro@studio.unibo.it>
2021-11-18 10:47:06 +0100

    Updated index page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="container">
       <c-flex direction="column" align="center">
-            <c-heading as="h1" size="2xl">Vuoi farla geolocalizzarla? Allora questo sito lo devi usarlo</c-heading>
-            <c-heading as="h2" size="md">qua puoi vedere i tweet filtrarli geolocalizzarli tutto fra</c-heading>
-            <c-heading as="h1" size="2xl">☀☕</c-heading>
-            <c-heading as="h3" size="md">clicca su "Tweets" che sta là la vera app</c-heading>
-            <c-image src="https://2.bp.blogspot.com/-F4jszpW691M/WC91ORuwsoI/AAAAAAAAB-c/rtFnTYmQRLAdv4ThqRYj8qLoWmtEcg6egCLcB/s1600/Animated-GIF-Banana.gif"></c-image>
+            <c-heading as="h1" size="2xl">Mordecai</c-heading>
+            <c-heading as="h2" size="md">A Twitter tracker for every taste</c-heading>
+            <c-image :src="require('~/assets/img/Mordecai.svg')" size="md"></c-image>
       </c-flex>
   </div>
 </template>
