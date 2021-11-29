f8d884634d27e865c59a72a8b7568f74ddfca3fe
matteo00111 <matteo.vannucchi11@gmail.com>
2021-11-29 13:30:45 +0100

    Termcloud finished

--- components/TermCloud.vue
+++ components/TermCloud.vue
@@ -4,20 +4,38 @@
       style="height: 480px; width: 640px;"
       :words="words"
       :color="color"
-      :font-family="fontFamily"
-      :font-size-ratio="fontSizeRatio"
-    />
+      :font-family="'Lucida Console'"
+      :font-size-ratio="5"
+      :spacing="0.2"
+      :rotation="rotation"
+      :animation-duration="1500"
+      :enter-animation="enterAnimation"
+    >
+      <template slot-scope="{text, weight, word}">
+        <div :title="weight" style="cursor: pointer;" @click="onWordClick(word)" @mouseover="onHover(word)">
+          {{ text }}
+        </div>
+      </template>
+    </vue-word-cloud>
+    <div v-if="!!hoverWord" id="infoTermCloud">
+      <c-box>
+        Parola: {{ hoverWord[0] }}, Peso: {{ hoverWord[1] }}
+      </c-box>
+    </div>
   </div>
 </template>
 
 <script>
 import VueWordCloud from 'vuewordcloud'
+import Chance from 'chance'
 
 function calculateColor ([word, weight]) {
-  if (weight > 50) { return 'Aqua' }
-  if (weight > 30) { return 'BlueViolet' }
-  if (weight > 20) { return 'DarkBlue' }
-  if (weight > 15) { return 'Green' } else { return 'Red' }
+  return weight > 40 ? '#ffd077' : weight > 17 ? '#3bc4c7' : weight > 10 ? '#3a9eea' : weight > 5 ? '#ff4e69' : '#461e47'
+}
+
+function calculateRotation (word) {
+  const chance = new Chance(word[0])
+  return chance.pickone([0, 1 / 8, 3 / 4, 7 / 8])
 }
 
 export default {
@@ -30,31 +48,31 @@ export default {
       type: Array,
       default: () => []
     },
-    fontFamily: {
-      type: String,
-      default: 'Baloo Bhaijaan'
-    },
     color: {
       type: Function,
       default: calculateColor
     },
-    fontSizeRatio: {
-      type: Number,
-      default: 5
+    rotation: {
+      type: Function,
+      default: calculateRotation
     }
   },
   data () {
-    return {}
-  },
-  created () {
+    return {
+      hoverWord: undefined,
+      enterAnimation: {
+        opacity: 0,
+        transform: 'scale3d(0.3,0.3,0.3)'
+      }
+    }
   },
   methods: {
+    onWordClick (word) {
+      this.$emit('onWordClick', word)
+    },
+    onHover (word) {
+      this.hoverWord = word
+    }
   }
 }
 </script>
-
-<style lang="
-      css"
-      scoped
-    >
-</style>
